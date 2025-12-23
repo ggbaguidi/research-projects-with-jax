fca4627e92e4102eb8a92aaf487213f947e59337
Ahonakpon Guy Gbaguidi <guygbaguidi123root@gmail.com>
2025-12-23 22:58:26 +0100

    feat: update __init__.py docstring for domain entities clarification

--- src/jax_deep_learning/core/domain/entities/__init__.py
+++ src/jax_deep_learning/core/domain/entities/__init__.py
@@ -1,4 +1,8 @@
-from dataset import *
-from model import *
+"""Domain entities.
+
+These are pure-ish structures used by the core. Keep filesystem/network I/O in adapters.
+"""
 
 from .base import *
+from .dataset import *
+from .model import *
